f60d85f8762072ae796fcbd284944084e43cf851
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-14 16:59:13 +0500

    Write os code

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,6 @@
+import os from "os";
+
 console.log("Hello from Node.js server!");
+console.log("Hostname:", os.hostname());
+console.log("Platform:", os.platform());
+console.log("Architecture:", os.arch());
