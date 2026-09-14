e3e0c6fadc294e7ea1e3c3483ddd5f55bf7d19d8
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-14 17:02:35 +0500

    Use fs module to create output file

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,16 @@
 import os from "os";
+import fs from "fs";
 
 console.log("Hello from Node.js server!");
+
 console.log("Hostname:", os.hostname());
 console.log("Platform:", os.platform());
 console.log("Architecture:", os.arch());
+
+fs.writeFile("output.txt", "This is a sample output from Node.js server.", (err) => {
+  if (err) {
+    console.error("Error writing to file:", err);
+  } else {
+    console.log("Output written to output.txt");
+  }
+});
