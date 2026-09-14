8b28dad850f8bb7aef0c790875a5249eb8b4dec5
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-14 17:09:57 +0500

    Create first Http Server

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,27 @@
 import os from "os";
 import fs from "fs";
+import http from "http";
 
-console.log("Hello from Node.js server!");
+// console.log("Hello from Node.js server!");
 
-console.log("Hostname:", os.hostname());
-console.log("Platform:", os.platform());
-console.log("Architecture:", os.arch());
+// console.log("Hostname:", os.hostname());
+// console.log("Platform:", os.platform());
+// console.log("Architecture:", os.arch());
 
-fs.writeFile("output.txt", "This is a sample output from Node.js server.", (err) => {
-  if (err) {
-    console.error("Error writing to file:", err);
-  } else {
-    console.log("Output written to output.txt");
-  }
+// fs.writeFile("output.txt", "This is a sample output from Node.js server.", (err) => {
+//   if (err) {
+//     console.error("Error writing to file:", err);
+//   } else {
+//     console.log("Output written to output.txt");
+//   }
+// });
+
+const Server = http.createServer((req, res) => {
+  res.end("Hello from Node.js server!");
+});
+
+const PORT = 3000;
+
+Server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
