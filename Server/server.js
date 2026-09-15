aa40fd1c1e17ab74245756f2882f2479493a0c89
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-15 06:36:14 +0500

    Format code

--- Server/server.js
+++ Server/server.js
@@ -1,17 +1,21 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-const PORT = 3000
+const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from Node.js server!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello from Node.js server!");
+});
 
-app.get("/about", (req, res) => { res.send("This is the About route"); });
+app.get("/about", (req, res) => {
+  res.send("This is the About route");
+});
 
-app.get("/api/interns", (req, res) => { res.json({ interns: ["Ali", "Sara", "Bilal"] }); });
+app.get("/api/interns", (req, res) => {
+  res.json({ interns: ["Ali", "Sara", "Bilal"] });
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
