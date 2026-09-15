0ec0e79568c3725d3b0fafde517a039a381537ea
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-15 06:35:48 +0500

    Create /api/interns route

--- Server/server.js
+++ Server/server.js
@@ -10,6 +10,8 @@ app.get('/', (req, res) => {
 
 app.get("/about", (req, res) => { res.send("This is the About route"); });
 
+app.get("/api/interns", (req, res) => { res.json({ interns: ["Ali", "Sara", "Bilal"] }); });
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
