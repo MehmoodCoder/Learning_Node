420c4d2e10768c6db1f2d9c93baee0dd8262eac6
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-15 06:35:02 +0500

    Create /about route

--- Server/server.js
+++ Server/server.js
@@ -8,6 +8,8 @@ app.get('/', (req, res) => {
   res.send('Hello from Node.js server!')
 })
 
+app.get("/about", (req, res) => { res.send("This is the About route"); });
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
