b4250f96003f7fec541bbfe8046a9a5e07e3a625
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-16 06:26:12 +0500

    Create a post route for create new task

--- Server/server.js
+++ Server/server.js
@@ -11,6 +11,9 @@ let tasks = [
 
 app.get("/api/tasks", (req, res) => { res.json(tasks); });
 
+app.post("/api/tasks", (req, res) => { const newTask = { id: tasks.length + 1, title: req.body.title, done: false
+}; tasks.push(newTask); res.status(201).json(newTask); });
+
 app.get("/", (req, res) => {
   res.send("Hello from Node.js server!");
 });
