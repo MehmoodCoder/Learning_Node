ab9ce528b2710d88d09099ec57b1acf80adab40a
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-22 16:56:27 +0500

    Create jsx component to list tasks

---
+++ client/src/components/Tasks.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import { getTasks } from "../api/tasksApi";
+
+export default function TaskList() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    getTasks().then((res) => setTasks(res.data));
+  }, []);
+
+  return (
+    <div>
+      <h2>Task List</h2>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task._id}>{task.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
