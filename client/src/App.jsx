74c0577df1c113a023fe1ba7b079784d93c7515a
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-22 17:04:55 +0500

    Render Add Task form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import TaskList from "./components/Tasks";
+import AddTask from './components/AddTask'
 
 function App() {
   return (
     <>
       <div className="">
         <TaskList />
+        <br />
+        <AddTask />
       </div>
     </>
   );
