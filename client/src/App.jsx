d1311f0a547d7f53e52eec5af4bf65596a2207b6
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-24 17:06:39 +0500

    Resolve error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TaskList from "./components/Tasks";
 import Login from "./pages/Login";
 import ProtectedRoute from "./pages/ProtectedRoute";
+import Header from "./components/Header";
+import Register from "./pages/Register";
 
 export default function App() {
   return (
+    <>
+      <Header/>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route
           path="/dashboard"
           element={
@@ -16,5 +21,6 @@ export default function App() {
           }
         />
       </Routes>
+      </>
   );
 }
