0184a7e3567dc7de7c251bb2b06d9325ec3ee0b5
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-24 07:13:22 +0500

    Wrap in Provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { AuthProvider } from "./context/AuthContext.jsx";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import { AuthProvider } from './context/AuthContext.jsx';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </AuthProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
