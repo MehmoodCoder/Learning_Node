5611a6e7b5b39864dcb3e469e5b00dacf753eade
MehmoodCoder <mehmood.mh56@gmail.com>
2026-09-25 19:31:37 +0500

    Add lazy loading and suspense for performance optimization

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,30 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import TaskList from "./components/Tasks";
-import Login from "./pages/Login";
-import ProtectedRoute from "./pages/ProtectedRoute";
 import Header from "./components/Header";
-import Register from "./pages/Register";
+
+const TaskList = lazy(() => import("./components/Tasks"));
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const ProtectedRoute = lazy(() => import("./pages/ProtectedRoute"));
 
 export default function App() {
   return (
-    <>
-      <Header/>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <TaskList />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-      </>
+    <BrowserRouter>
+      <Header />
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <TaskList />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
